3a1cc0d15e5b0dfa576f7a55f64ad34a5002e59a
devomaa <25231103+devomaa@users.noreply.github.com>
2020-03-06 16:47:47 +0200

    Inital Commit
    Whats a repository with no files?

---
+++ installer.lua
@@ -0,0 +1,26 @@
+
+term.clear()
+term.setCursorPos(1,1)
+function input()
+	term.write("OpusX Installer, Type install to install, type exit to exit.")
+	inputt = read()
+end
+input()
+if inputt == "install" then
+	shell.run("git https://github.com/BDragonCC/OpusX.git")
+	fs.move("OpusX/*", /*")
+	if fs.exists("startup.lua")
+		print("Rebooting in 3 seconds")
+		sleep(3)
+		os.reboot()
+	else
+		print("An unknown error has occured, is GitHub down?")
+		error("Unknown")
+	end
+elseif inputt == "exit" then
+	error("Exited by user.", 0)
+else
+	input()
+end
+
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+#opusx-installer
+This is the OpusX Installer. Normally you wouldn't come here.
